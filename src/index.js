80457d7a3dc04554c1ece01eb75ef29b8cf7d7e4
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2020-12-29 13:27:11 -0500

    imported bcrypt to has passwords

--- src/index.js
+++ src/index.js
@@ -17,3 +17,18 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
+// const bcrypt = require('bcryptjs');
+
+// const myFunction = async () => {
+//     const password = 'Red12345';
+//     const hashPassword = await bcrypt.hash(password, 8);
+
+//     console.log(password);
+//     console.log(hashPassword);
+
+//     const isMatch = await bcrypt.compare("Re12345", hashPassword);
+//     console.log(isMatch);
+// }
+
+// myFunction()
