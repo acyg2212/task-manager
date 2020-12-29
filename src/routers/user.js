d4c38c9af8740cecfb186f8478034d1277cd1f07
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2020-12-29 15:58:27 -0500

    registering user and logging user in can now generate json web tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,12 +7,24 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
 
 });
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken()
+        res.send({ user, token });
+
+    } catch (e) {
+        res.status(400).send();
+    }
+})
 
 router.get('/users', async (req, res) => {
 
