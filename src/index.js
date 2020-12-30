3732b609f9725b32f81a0693a4a55d8930bef9e3
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2020-12-29 23:02:46 -0500

    ability to add an avatar

--- src/index.js
+++ src/index.js
@@ -10,6 +10,29 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'test',
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        if (!file.originalname.match(/\.(doc | docx)$/)) {
+            return cb(new Error('Please upload a Word Document'))
+        }
+        cb(undefined, true);
+    }
+})
+
+
+app.post("/", upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
